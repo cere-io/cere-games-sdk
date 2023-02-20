b3248bc2cc8610c5f1756bd76b374e4947cf931f
Sergey Kambalin <sergey.kambalin@cere.io>
2023-02-20 12:37:42 +0600

    Build Games SDK UI lib for browser

---
+++ packages/ui/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: { name: 'CereGamesSdkUI', type: 'umd' },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.png/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
