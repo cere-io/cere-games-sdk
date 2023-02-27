508da3a50414b3e684867c9555a5af32b37c9bff
Sergey Kambalin <sergey.kambalin@cere.io>
2023-02-27 16:48:41 +0600

    Enable modal scrolling

--- packages/ui/src/components/Backdrop/Backdrop.tsx
+++ packages/ui/src/components/Backdrop/Backdrop.tsx
@@ -1,12 +1,28 @@
+import { PropsWithChildren } from 'react';
 import styled from '@emotion/styled';
 
-export const Backdrop = styled.div({
-  position: 'absolute',
+export type BackdropProps = PropsWithChildren<{}>;
+
+const Overlay = styled.div({
+  position: 'fixed',
   top: 0,
-  width: '100vw',
-  height: '100vh',
-  backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  right: 0,
+  bottom: 0,
+  left: 0,
   display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  flexDirection: 'column',
+  overflow: 'auto',
+  backgroundColor: 'rgba(0, 0, 0, 0.8)',
+  WebkitTapHighlightColor: 'transparent',
 });
+
+const Content = styled.div({
+  flexShrink: 0,
+  margin: 'auto',
+});
+
+export const Backdrop = ({ children }: BackdropProps) => (
+  <Overlay>
+    <Content>{children}</Content>
+  </Overlay>
+);
