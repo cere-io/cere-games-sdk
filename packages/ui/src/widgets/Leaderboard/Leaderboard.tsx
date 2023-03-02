e3bc05c9af3c732001d901d7f4b2ecc800fff076
antonmazuto <mazhutoa@appclose.com>
2023-03-02 17:43:12 +0300

    feature Nft-reward

--- packages/ui/src/widgets/Leaderboard/Leaderboard.tsx
+++ packages/ui/src/widgets/Leaderboard/Leaderboard.tsx
@@ -23,7 +23,7 @@ export const Leaderboard = ({ data, onPlayAgain }: LeaderboardProps) => {
   return (
     <Widget>
       <Stack direction="row" spacing={2} margin={[0, 0, 3]}>
-        <NftReward data={data} address={'0x2350d687a8bf2e2be0ef5a28940b05107d79211d'} />
+        <NftReward data={data} address={address} />
       </Stack>
       <Stack direction="row" spacing={2} margin={[0, 0, 4]}>
         <Button icon={<RepeatIcon />} onClick={onPlayAgain}>
