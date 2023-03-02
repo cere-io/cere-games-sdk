0e183d22cd6a29985ef0b8be7e2f442acada4c82
Sergey Kambalin <sergey.kambalin@cere.io>
2023-03-02 18:39:31 +0600

    Games SDK UMD build

--- packages/sdk/webpack.config.js
+++ packages/sdk/webpack.config.js
@@ -1,14 +1,19 @@
 const path = require('path');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.ts',
+
   output: {
     clean: true,
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.umd.js',
-    library: { name: 'CereGamesSdkUI', type: 'umd' },
+    library: { name: 'CereGamesSDK', type: 'umd' },
   },
+
+  plugins: [new NodePolyfillPlugin()],
+
   module: {
     rules: [
       {
@@ -21,12 +26,9 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.png/,
-        type: 'asset/inline',
-      },
     ],
   },
+
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
