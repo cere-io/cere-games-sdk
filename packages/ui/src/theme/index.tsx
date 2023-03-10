20da26da64475a6afeddb200eeeff01c4ba0bb6d
antonmazuto <mazhutoa@appclose.com>
2023-03-10 07:34:51 +0300

    Fix the connect wallet modal according to the new design

--- packages/ui/src/theme/index.tsx
+++ packages/ui/src/theme/index.tsx
@@ -1,23 +1,16 @@
 import { useMemo } from 'react';
-import { ThemeProvider as Provider, ThemeProviderProps, Global, css } from '@emotion/react';
+import { ThemeProvider as Provider, ThemeProviderProps, Global } from '@emotion/react';
 
 import { createTheme } from './createTheme';
 
 export * from './types';
-
-const GlobalStyles = css`
-  @import url('https://fonts.googleapis.com/css2?family=Lexend&display=swap');
-  * {
-    font-family: 'Lexend', sans-serif;
-  }
-`;
 
 export const ThemeProvider = ({ children, ...props }: Omit<ThemeProviderProps, 'theme'>) => {
   const theme = useMemo(() => createTheme(), []);
 
   return (
     <Provider theme={theme} {...props}>
-      <Global styles={{ ...theme.global, GlobalStyles }} />
+      <Global styles={theme.global} />
 
       {children}
     </Provider>
