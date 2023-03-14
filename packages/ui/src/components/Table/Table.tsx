3bae1aff17856f0999767970ec987a6bcd31ae1f
antonmazuto <mazhutoa@appclose.com>
2023-03-14 20:42:44 +0300

    Fix the connect wallet modal according to the new design

--- packages/ui/src/components/Table/Table.tsx
+++ packages/ui/src/components/Table/Table.tsx
@@ -15,14 +15,12 @@ export const Table = ({ data, activeAddress }: TableProps) => {
   const activeRow = useMemo(() => data.find((row) => row.address === activeAddress), [data, activeAddress]);
   const rows = useMemo(() => data.filter((row) => row !== activeRow), [activeRow, data]);
 
-  console.log(rows);
-
   return (
     <Container>
       <TableHeader columns={['Rank', 'Player', 'Prize', 'Score']} />
       {activeRow && <TableDataRow active data={activeRow} />}
-      {rows.map((row, idx) => (
-        <TableDataRow key={row.address} showPrize={idx <= 20} data={row} />
+      {rows.map((row) => (
+        <TableDataRow key={row.address} data={row} />
       ))}
     </Container>
   );
