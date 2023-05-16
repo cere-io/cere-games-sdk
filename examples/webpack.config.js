6229b5d8a4a28b781570d076e9e4ba6da7b46bad
Sergey Kambalin <sergey.kambalin@cere.io>
2023-05-16 10:50:54 +0600

    Examples build variables

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -4,6 +4,11 @@ const CopyPlugin = require('copy-webpack-plugin');
 const buildDir = path.resolve(__dirname, '../build');
 const publicDir = path.resolve(__dirname, 'public');
 
+const injectVars = (vars) => (content) =>
+  Object.entries(vars).reduce((content, [name, value]) => {
+    return content.replaceAll(`%${name}%`, value);
+  }, content.toString());
+
 module.exports = {
   mode: 'production',
   entry: {
@@ -21,7 +26,16 @@ module.exports = {
 
   plugins: [
     new CopyPlugin({
-      patterns: [{ from: publicDir }],
+      patterns: [
+        { context: publicDir, from: '**/*' },
+        {
+          context: publicDir,
+          from: '**/*.html',
+          transform: injectVars({
+            BUILD_TIME: new Date().getTime(),
+          }),
+        },
+      ],
     }),
   ],
 
