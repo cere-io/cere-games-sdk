a19dd3a63bcc9a5e751694709df198b092520bd9
Sergey Kambalin <sergey.kambalin@cere.io>
2023-05-16 18:20:12 +0600

    Remove not working startSelenoid option

--- tests/wdio/config/selenoid.ts
+++ tests/wdio/config/selenoid.ts
@@ -2,28 +2,13 @@ import { config as baseConfig } from './base';
 import { chromeCapability } from './chromeDriver';
 import { options } from './options';
 
-const services: WebdriverIO.Config['services'] = [...baseConfig.services];
-
-if (options.startSelenoid) {
-  services.push([
-    'selenoid-standalone',
-    {
-      skipAutoPullImages: false,
-      pathToBrowsersConfig: './browsers.json',
-      selenoidArgs: ['-limit', '10'],
-    },
-  ]);
-}
-
 export const config: WebdriverIO.Config = {
   ...baseConfig,
 
-  baseUrl: 'http://host.docker.internal:4567/examples/',
   hostname: 'localhost',
   port: 4444,
   path: '/wd/hub/',
 
-  services,
   capabilities: [
     {
       ...chromeCapability,
