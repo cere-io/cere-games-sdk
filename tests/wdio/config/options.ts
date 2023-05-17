55a8c9243d60124d440caa9346e872dd4b7f7328
Sergey Kambalin <sergey.kambalin@cere.io>
2023-05-17 11:08:45 +0600

    Update wdi config

--- tests/wdio/config/options.ts
+++ tests/wdio/config/options.ts
@@ -2,7 +2,7 @@ import { hideBin } from 'yargs/helpers';
 import yargs from 'yargs';
 
 export const options = yargs(hideBin(process.argv)).options({
-  variant: { choices: ['selenoid', 'chrome'] as const },
+  ci: { type: 'boolean', default: false },
   headless: { type: 'boolean', default: false },
   maxInstances: { type: 'number', default: 1 },
 }).argv;
