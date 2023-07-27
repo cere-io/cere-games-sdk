00cb6fc0daae0ce89802a5a3bd7fe5c819454304
Kukhty, Rodion <rodion.kuhty@solveit.dev>
2023-07-27 12:04:29 +0300

    fix: fix closing of the alert

--- packages/ui/src/components/Alert/Alert.tsx
+++ packages/ui/src/components/Alert/Alert.tsx
@@ -1,34 +1,38 @@
 import styled from '@emotion/styled';
 import { useId, useState } from 'react';
-import { keyframes } from '@emotion/react';
 
 import { Typography } from '../Typography';
 import { CloseIcon } from '../../icons';
-import { useConfigContext, useWalletContext } from '../../hooks';
+import { useConfigContext, useWalletContext, useMediaQuery } from '../../hooks';
 
-const Container = styled.div({
-  zIndex: 100,
-  position: 'absolute',
-  right: 39,
-  top: 32,
-  display: 'grid',
-  gridTemplateColumns: 'auto auto 32px',
-  columnGap: 12,
-  alignItems: 'center',
-  padding: '12px 16px',
-  background: 'linear-gradient(180deg, #010107 23.96%, #2C325B 100%)',
-  boxShadow: '0px 3px 8px rgba(0, 0, 0, 0.1)',
-  borderRadius: 24,
-  '& > span': {
-    fontSize: 32,
-  },
+const Container = styled.div<{ visible: boolean }>(
+  {
+    zIndex: 100,
+    position: 'absolute',
+    right: 39,
+    top: 32,
+    display: 'grid',
+    gridTemplateColumns: 'auto auto 32px',
+    columnGap: 12,
+    alignItems: 'center',
+    padding: '12px 16px',
+    background: 'linear-gradient(180deg, #010107 23.96%, #2C325B 100%)',
+    boxShadow: '0px 3px 8px rgba(0, 0, 0, 0.1)',
+    borderRadius: 24,
+    '& > span': {
+      fontSize: 32,
+    },
 
-  '@media (max-width: 600px)': {
-    top: 0,
-    right: 0,
-    width: '100%',
+    '@media (max-width: 600px)': {
+      top: 0,
+      right: 0,
+      width: '100%',
+    },
   },
-});
+  ({ visible }) => ({
+    visibility: visible ? 'visible' : 'hidden',
+  }),
+);
 
 const CloseIconWrapper = styled.div({
   position: 'absolute',
@@ -44,23 +48,6 @@ const CloseAlert = styled(CloseIcon)(() => ({
   boxSizing: 'content-box',
   cursor: 'pointer',
 }));
-
-const StyledTypography = styled(Typography)({
-  maxWidth: 220,
-  '@media (max-width: 600px)': {
-    maxWidth: '100%',
-  },
-});
-
-const ProgressContainer = styled.div({
-  position: 'absolute',
-  bottom: 0,
-  height: 3,
-  width: '97%',
-  borderRadius: '0 0 12px 12px',
-  marginLeft: 6,
-  background: '#D6D6D6',
-});
 
 const TextContainer = styled.div({
   display: 'flex',
@@ -97,49 +84,19 @@ const SubTitle = styled(Typography)({
   marginTop: 4,
 });
 
-const ProgressBar = styled.div(() => ({
-  animationName: keyframes({
-    from: {
-      width: 0,
-    },
-
-    to: {
-      width: '100%',
-    },
-  }),
-
-  animationDuration: '5s',
-  animationIterationCount: 1,
-  transitionProperty: 'width',
-  animationTimingFunction: 'linear',
-
-  display: 'block',
-  width: 0,
-  borderRadius: '0 0 12px 12px',
-  height: 3,
-  background: 'linear-gradient(79.06deg, #E34DC5 0%, #9227E3 100%)',
-  position: 'relative',
-  '&:after': {
-    content: '""',
-    zIndex: '1',
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-  },
-}));
-
 export const Alert = () => {
   const descriptionId = useId();
   const { sdkUrl: cdnUrl } = useConfigContext();
   const { balance, isNewUser } = useWalletContext();
   const [visible, setVisibility] = useState(true);
+  const isMobile = useMediaQuery('(max-width: 600px)');
 
-  if (!visible || !balance || !isNewUser) {
+  if (!visible || !balance || !isNewUser || isMobile) {
     return null;
   }
 
   return (
-    <Container role="alert" aria-describedby={descriptionId}>
+    <Container role="alert" aria-describedby={descriptionId} visible={visible}>
       <StyledImage src={`${cdnUrl}/assets/alert.png`} />
       <TextContainer>
         <Title id={descriptionId} variant="body2">
@@ -149,14 +106,12 @@ export const Alert = () => {
           NeWBIE way
         </SubTitle>
       </TextContainer>
-      <CloseIconWrapper>
-        <CloseAlert
-          aria-label="Close"
-          onClick={() => {
-            console.log('asdasfd');
-            setVisibility(false);
-          }}
-        />
+      <CloseIconWrapper
+        onClick={() => {
+          setVisibility(false);
+        }}
+      >
+        <CloseAlert aria-label="Close" />
       </CloseIconWrapper>
     </Container>
   );
